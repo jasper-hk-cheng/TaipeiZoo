fc72c95486df5bdf17d919c408a73a34df5d5f42
Jasper <jasper.hk.cheng@gmail.com>
2021-04-06 03:36:48 +0800

    排版以及移除不必要的註解

--- app/src/main/java/com/cathay/banc/taipei/zoo/di/DiModule.kt
+++ app/src/main/java/com/cathay/banc/taipei/zoo/di/DiModule.kt
@@ -8,7 +8,7 @@ import org.koin.dsl.module
 
 val arenaPresenterModule = module {
     single<ArenaPresenter> { (view: ZooContract.IArenaView) ->
-        ArenaPresenter(androidContext() ,view)
+        ArenaPresenter(androidContext(), view)
     }
 }
 
